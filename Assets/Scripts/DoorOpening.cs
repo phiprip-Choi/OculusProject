7544c9c74790bbf175e5d3ecd2ef274f2c2a34c5
Phiprip-Choi <132041240+phiprip-Choi@users.noreply.github.com>
2024-04-19 00:12:46 +0900

    Edit| 회전문 추가

--- Assets/Scripts/DoorOpening.cs
+++ Assets/Scripts/DoorOpening.cs
@@ -4,6 +4,9 @@ using UnityEngine;
 
 public class DoorOpening : MonoBehaviour
 {
+
+    public float degree = 60f;
+    public bool isX = true;
     // Update is called once per frame
     void Update()
     {
@@ -16,11 +19,13 @@ public class DoorOpening : MonoBehaviour
 
     IEnumerator RotateDoor()
     {
-        while (transform.rotation.eulerAngles.x < 60f)
+
+        do
         {
-            transform.rotation = Quaternion.Euler( new Vector3(transform.rotation.eulerAngles.x + 0.5f, 0, 0));
+            Debug.Log(transform.localRotation.eulerAngles.z);
+            transform.localRotation = Quaternion.Euler(new Vector3(0, 0, transform.localEulerAngles.z - 0.5f));
             yield return new WaitForFixedUpdate();
             //transform.Rotate(new Vector3(0.005f * Time.deltaTime, 0, 0));
-        } 
+        } while (transform.localRotation.eulerAngles.z > 360f + degree);
     }
 }
