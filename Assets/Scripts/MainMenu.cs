459f12f47cda902e3582de5f89bf0d0926878442
fadnot123 <150272512+fadnot123@users.noreply.github.com>
2024-05-13 14:59:50 +0900

    UI_페이드추가 & 불필요 스크립트 제거(quit, scencemanage, debug)

--- Assets/Scripts/MainMenu.cs
+++ Assets/Scripts/MainMenu.cs
@@ -18,7 +18,11 @@ public class MainMenu : MonoBehaviour
     }
     public void GameScnesCtrl()
     {
-        SceneManager.LoadScene("Stage1"); //이동할 씬 이름이 같아야 함.
+        OVRScreenFade.instance.FadeOut(() =>
+        {
+            SceneManager.LoadScene("Stage1"); //이동할 씬 이름이 같아야 함.
+        });
+        
         Debug.Log("move");
     }
     public void OnClickNewGame()
