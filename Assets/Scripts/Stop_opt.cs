461813d11f2b17fcc2054be712a6e7f72c764462
fadnot123 <xodn8596@naver.com>
2024-05-30 05:22:28 +0900

    옵션 UI 개선

--- Assets/Scripts/Stop_opt.cs
+++ Assets/Scripts/Stop_opt.cs
@@ -17,7 +17,7 @@ public class MainMenu_ : MonoBehaviour
     public Button back_btn = null;
     public string MainMenuSceneName;
 
-    public float distanceFromPlayer = 1.0f;
+    public float distanceFromPlayer = 0.1f;
     public GameObject player;
 
     private void Start()
@@ -34,6 +34,11 @@ public class MainMenu_ : MonoBehaviour
 
     private void Update()
     {
+        if (Canvas_opt.activeSelf)
+        {
+            return;
+        }
+
         if (OVRInput.GetDown(OVRInput.Button.Three))
         {
             if (OptionMenu.activeSelf)
@@ -58,23 +63,28 @@ public class MainMenu_ : MonoBehaviour
     }
     private void OptionCanvas()
     {
+        
         OptionMenu.SetActive(false);
         Canvas_opt.SetActive(true);
+        
     }
 
     private void OnClickBack()
     {
+        
         OptionMenu.SetActive(true);
         Canvas_opt.SetActive(false);
     }
 
     private void GameRetry()
     {
+        Time.timeScale = 1;
         SceneManager.LoadScene(SceneManager.GetActiveScene().name); //GetActiveScene은 해당 신의 이름을 가져온다.
     }
 
     private void LoadMainMenu()
     {
+        Time.timeScale = 1;
         OVRScreenFade.instance.FadeOut(() =>
         {
             SceneManager.LoadScene(MainMenuSceneName); //이동할 씬 이름이 같아야 함.
@@ -83,6 +93,7 @@ public class MainMenu_ : MonoBehaviour
 
     private void GameQuit()
     {
+        Time.timeScale = 1;
 #if UNITY_EDITOR
         UnityEditor.EditorApplication.isPlaying = false;
 #else
